a3ed8dc83e9d231f4ea7c53274269a45e3346f35
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-11 13:45:51 +0530

    learn how to get location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -20,6 +20,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
+            getLocation();
             //Get the current location
           },
           child: Text('Get Location'),
