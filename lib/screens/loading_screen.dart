60e897fc84d9b74e33160c5eda73fc49fa99c9b4
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-12 20:02:38 +0530

    learning how to use APIs in Flutter

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -11,8 +11,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
 
   void initState() {
-    getLocation();
     super.initState();
+    getLocation();
+    print('this line of code is triggered');
+    getData();
   }
 
   void getLocation() async {
@@ -21,7 +23,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
     print(location.longitude);
 
-}
+  }
+
+  void getData() async{
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02')''
+    print(response);
+  }
 
 
   Widget build(BuildContext context) {
