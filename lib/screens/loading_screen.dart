1e1a172de98fcab4d8f81224bf57a79bb684eae4
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-13 16:33:10 +0530

    learning how to get dummy data from api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:clima/services/location.dart';
@@ -26,8 +28,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async{
-    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02')''
-    print(response);
+    http.Response response = await
+    http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode==200){
+      String data = response.body;
+      print(data);
+
+      var decodeData = jsonDecode(data);
+
+      var temperature = decodeData['main']['temp'];
+      var condition = decodeData['weather'][0]['id'];
+      var cityName = decodeData['name'];
+
+
+    } else{
+      print(response.statusCode);
+    }
   }
 
 
