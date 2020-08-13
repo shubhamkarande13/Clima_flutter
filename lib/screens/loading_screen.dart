552d8fa829df7bb06a4ff51cd624a2a5d2cb4e6f
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-13 18:23:01 +0530

    refactor getWeatherData() code

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:clima/services/location.dart';
@@ -13,49 +13,24 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  @override
 
   double latitude;
   double longitude;
 
   void initState() {
     super.initState();
-    getLocation();
-    print('this line of code is triggered');
-    getData();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location=Location();
     await location.getCurrentLocation();
     latitude=location.latitude;
     longitude=location.longitude;
-    getData();
 
+    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
   }
-
-  void getData() async{
-    http.Response response = await
-    http.get('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-    if (response.statusCode==200){
-      String data = response.body;
-      //print(data);
-
-      var decodeData = jsonDecode(data);
-
-      var temperature = decodeData['main']['temp'];
-      var condition = decodeData['weather'][0]['id'];
-      var cityName = decodeData['name'];
-
-      print (temperature);
-      print (condition);
-      print(cityName);
-
-    } else{
-      print(response.statusCode);
-    }
-  }
-
 
   Widget build(BuildContext context) {
     return Scaffold();
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -74,3 +74,8 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
+
+//
+//var temperature = decodeData['main']['temp'];
+//var condition = decodeData['weather'][0]['id'];
+//var cityName = decodeData['name'];
